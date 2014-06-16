adccc1fa25904e00913e2dbec47063968ce5498d
sailorhero <sailorhero@163.com>
2014-06-16 17:05:50 +0000

    modify ci shell: fix con't open error

--- buildcc.sh
+++ buildcc.sh
@@ -1,5 +1,3 @@
-#!/bin/sh 
-
 BuildPath=build
 CPPCheckReport=cppcheck.xml
 CPPCheckApp=`which cppcheck`
@@ -8,25 +6,20 @@ CPPNCSSApp=`which cppncss`
 GCOVReport=gcovreport.xml
 GCOVRApp=`which gcovr`
 
-echo "BuildPath:$BuildPath"
+SRCDir=src
+
 #GTestReport=gtestreport.xml
-if test -d "$BuildPath"; then 
-     cd $BuildPath 
-     echo "clean older build files "
+mkdir $BuildPath
+echo "clean older build files "
+cd $BuildPath
+make clean
+rm build/*.gcov 		 	> /dev/null
+rm build/$CPPCheckReport 	> /dev/null
+rm build/$CPPNCSSReport 	> /dev/null
+rm build/$GCOVReport 		> /dev/null
+rm -rf build/xunit 			> /dev/null  
 
-     make clean
-     #rm -rf build
-     rm $BuildPath/*.gcov               > /dev/null
-     rm $BuildPath/$CPPCheckReport      > /dev/null
-     rm $BuildPath/$CPPNCSSReport  > /dev/null
-     rm $BuildPath/$GCOVReport          > /dev/null
-     rm -rf $BuildPath/xunit            > /dev/null  
-else
-     mkdir "$BuildPath"
-     cd $BuildPath
-fi
-
-cmake  ..
+cmake ..
 export CPPFLAGS=" -fprofile-arcs -ftest-coverage"
 export LDFLAGS=" -lgcov"
 make
@@ -34,7 +27,7 @@ make
 if  test -n "$BuildPath" ; then
      echo "Make Success"
      echo "Make CPPCheck Report $CPPCheckReport"
-     $CPPCheckApp -v --enable=all --xml -I ../src -I ../libhello ../src  2> $CPPCheckReport
+     $CPPCheckApp -v --enable=all --xml -I ../libhello ../src  2> $CPPCheckReport
 
      echo "Make CCCC Report $CCCCReport"
      find ../src ../libhello -type f | grep -E '(*\.cc|*\.c|*\.hpp|*\.h|*\.cc)$' | xargs cccc --lang=c++ --outdir=.cccc
@@ -45,13 +38,14 @@ if  test -n "$BuildPath" ; then
      echo "Make sloccount Report "
      sloccount --duplicates --wide --details ../src ../libhello > sloccount.sc
      
-     echo "Run gtest Test Program, Make Test Report"
-     export GTEST_OUTPUT="xml:xunit/"
+     #echo "Run gtest Test Program, Make Test Report"
+     #export GTEST_OUTPUT="xml:xunit/"
      #make test
+     #$RunPath/vodservertest.out --gtest_output=xml:$GTestReport > /dev /null
      
      echo "Make GCovr"
-     #$GCOVRApp -x -r .. > $GCOVReport
-
+     $GCOVRApp -x  > $GCOVReport
+	 #-fprofile-arcs -ftest-coverage -fPIC
 else
      echo "Make Fail!"
 fi
