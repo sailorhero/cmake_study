97d19bbac9d250ec7f429d99ff943e286b1bbab0
sailorhero <sailorhero@163.com>
2014-06-24 13:49:39 +0800

    修改宏定义错误，主程序打印版本号
    1. cmake使用CMakelists.txt中定义的头文件更新宏定义文件
    2. 主程序调用宏定义，打印版本号

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 int main()
 {
 	printf("Welcome use hello program!\n");
-	/*
+	
 	printf("Version: %d.%d\n",
 		HELLO_VERSION_MAJOR,
 		HELLO_VERSION_MINOR);
-	*/
+
     char *str = hello("World");
     printf("%s!\n",str);
     return 0;
