cc7c2aed72c0752a67761dd65f2fec5f840e8427
sailorhero <sailorhero@163.com>
2014-06-25 00:33:45 +0800

    更新持续集成脚本

--- buildcc.sh
+++ buildcc.sh
@@ -17,7 +17,7 @@ cd $BuildPath
 make clean
 rm build/*.gcov 		 	> /dev/null
 rm build/$CPPCheckReport 	> /dev/null
-rm build/$CPPNCSSReport 	> /dev/null
+rm build/$CPPNCSSReport 	     > /dev/null
 rm build/$GCOVReport 		> /dev/null
 rm -rf build/xunit 			> /dev/null  
 
@@ -30,7 +30,7 @@ make
 if  test -n "$BuildPath" ; then
      echo "Make Success"
      echo "Make CPPCheck Report $CPPCheckReport"
-     $CPPCheckApp -v --enable=all --xml -I ../libhello ../src  2> $CPPCheckReport
+     $CPPCheckApp -v --enable=all --xml -I ../libhello -i../gtest-1.7.0 -i../build -itest ..  2> $CPPCheckReport
 
      echo "Make CCCC Report $CCCCReport"
      find ../src ../libhello -type f | grep -E '(*\.cc|*\.c|*\.hpp|*\.h|*\.cc)$' | xargs cccc --lang=c++ --outdir=.cccc
@@ -42,12 +42,12 @@ if  test -n "$BuildPath" ; then
      sloccount --duplicates --wide --details ../src ../libhello > sloccount.sc
      
      #echo "Run gtest Test Program, Make Test Report"
-     #export GTEST_OUTPUT="xml:xunit/"
-     #make test
-     #$RunPath/vodservertest.out --gtest_output=xml:$GTestReport > /dev /null
+     export GTEST_OUTPUT="xml:xunit/"
+     make test 
      
      echo "Make GCovr"
-     $GCOVRApp -x -e/usr/include -e$PROJECT_PATH/test > $GCOVReport
+     bin/hello
+     $GCOVRApp -x -e/usr/include -e$PROJECT_PATH/test -e$PROJECT_PATH/gtest-1.7.0 > $GCOVReport
 	 #-fprofile-arcs -ftest-coverage -fPIC
 else
      echo "Make Fail!"
